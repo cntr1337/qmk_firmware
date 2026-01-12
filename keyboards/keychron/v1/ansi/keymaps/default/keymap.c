3142236e6711179cf2cebcb76b3ed35c902bddf6
cntr1337 <blaszczynski.konrad@gmail.com>
2026-01-12 13:18:20 +0100

    zmiana kodu

--- keyboards/keychron/v1/ansi/keymaps/default/keymap.c
+++ keyboards/keychron/v1/ansi/keymaps/default/keymap.c
@@ -4,21 +4,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include QMK_KEYBOARD_H
 
 // clang-format off
 
-// 1. Definicja warstw
 enum layers{
     MAC_BASE,
     MAC_FN,
@@ -26,65 +17,81 @@ enum layers{
     WIN_FN
 };
 
-// 2. Definicja Twojego nowego przycisku
+// Definiujemy wlasny kod przelacznika
 enum custom_keycodes {
-    F_MOUSE = SAFE_RANGE,
+    TOGGLE_MOUSE = SAFE_RANGE,
 };
 
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
+// Zmienna pamietajaca stan (false = wylaczony, true = wlaczony)
+static bool mouse_mode_active = false;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Warstwa MAC_BASE - tutaj podmienilem KC_F na F_MOUSE */
+    /* MAC_BASE: Tutaj F jest zwykłym KC_F */
     [MAC_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_DEL,             KC_INS,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     F_MOUSE,  KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         KC_LCTL,  KC_LOPT,  KC_LCMD,                                            KC_SPC,                                 KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
+    /* MAC_FN: Tutaj podmienilem prawy skrajny klawisz (Home) na TOGGLE_MOUSE */
     [MAC_FN] = LAYOUT_ansi_82(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
+        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,       TOGGLE_MOUSE,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,                                            _______,                                _______,  _______,    _______,  _______,  _______,  _______),
 
-    /* Warstwa WIN_BASE - tutaj tez podmienilem KC_F na F_MOUSE */
+    /* WIN_BASE: F jest zwykłym KC_F */
     [WIN_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_INS,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     F_MOUSE,  KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                            KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
+    /* WIN_FN: Tutaj tez Home zmieniony na TOGGLE_MOUSE */
     [WIN_FN] = LAYOUT_ansi_82(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
+        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,       TOGGLE_MOUSE,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,                                            _______,                                _______,  _______,    _______,  _______,  _______,  _______),
 };
 
-// 3. Logika dzialania przycisku (dodana na koncu)
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case F_MOUSE:
+        // 1. Obsluga przelacznika (Fn + Home)
+        case TOGGLE_MOUSE:
             if (record->event.pressed) {
-                // Wcisniecie przycisku:
-                tap_code(KC_F);             // Kliknij raz F
-                register_code(KC_MS_BTN1);  // Wcisnij i trzymaj Lewy Przycisk Myszy
-            } else {
-                // Puszczenie przycisku:
-                unregister_code(KC_MS_BTN1); // Pusc Lewy Przycisk Myszy
+                mouse_mode_active = !mouse_mode_active; // Zmien stan na przeciwny
+                // Opcjonalnie: mozesz tu dodac migniecie LEDami, zeby wiedziec ze dziala
             }
-            return false; // Zastepujemy standardowe dzialanie
+            return false;
+
+        // 2. Obsluga klawisza F
+        case KC_F:
+            // Sprawdzamy czy tryb jest wlaczony
+            if (mouse_mode_active) {
+                if (record->event.pressed) {
+                    tap_code(KC_F);             // Kliknij F
+                    register_code(KC_MS_BTN1);  // Trzymaj Mysz
+                } else {
+                    unregister_code(KC_MS_BTN1); // Pusc Mysz
+                }
+                return false; // Zastepujemy standardowe F
+            }
+            // Jesli tryb jest WYLACZONY, nic nie robimy - F dziala jak zwykle F
+            return true;
             
         default:
-            return true; // Inne klawisze dzialaja normalnie
+            return true;
     }
 }
