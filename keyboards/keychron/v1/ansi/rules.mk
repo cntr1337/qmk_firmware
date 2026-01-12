ac4f0fbc4fcb2759da37068a4cf3006c459eb334
cntr1337 <blaszczynski.konrad@gmail.com>
2026-01-12 15:05:21 +0100

    Enhance rules.mk with comments and feature adjustments
    Added comments for clarity and disabled unused features.

--- keyboards/keychron/v1/ansi/rules.mk
+++ keyboards/keychron/v1/ansi/rules.mk
@@ -1,9 +1,13 @@
 MCU = STM32L432
 BOOTLOADER = stm32-dfu
 
-# Wlaczenie funkcji myszki
+# Wlaczenie funkcji myszki - BEZ TEGO NIE ZADZIALA
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 
-# Opcjonalnie wlaczenie VIA
+# Obsluga VIA (zeby widzialo klawiature)
 VIA_ENABLE = yes
+
+# Wylaczenie zbednych funkcji zeby zaoszczedzic miejsce
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
