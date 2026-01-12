7254c2e90181c9d7bd0b4350f5312dbd52ceac04
cntr1337 <blaszczynski.konrad@gmail.com>
2026-01-12 14:22:02 +0100

    Update keymap.c

--- keyboards/keychron/v1/ansi/keymaps/default/keymap.c
+++ keyboards/keychron/v1/ansi/keymaps/default/keymap.c
@@ -8,12 +8,10 @@
 
 #include QMK_KEYBOARD_H
 
-/* --- FIX NAPRAWIAJACY BLAD KOMPILACJI --- */
-/* Czasami kompilator gubi definicje myszki. Definiujemy je recznie. */
+/* FIX NAPRAWIAJACY BLAD KOMPILACJI (MYSZKA) */
 #ifndef KC_MS_BTN1
 #define KC_MS_BTN1 0x00F9
 #endif
-/* ---------------------------------------- */
 
 // clang-format off
 
@@ -24,7 +22,6 @@ enum layers{
     WIN_FN
 };
 
-// Definiujemy wlasny kod przycisku do wlaczania trybu
 enum custom_keycodes {
     TOGGLE_MOUSE = SAFE_RANGE,
 };
@@ -32,7 +29,6 @@ enum custom_keycodes {
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
-// Zmienna przechowujaca stan (false = wylaczony, true = wlaczony)
 static bool mouse_mode_active = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -43,14 +39,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LOPT,  KC_LCMD,                                            KC_SPC,                                 KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LOPT,  KC_LCMD,                                            KC_SPC,                                 KC_RCMD,  MO(MAC_FN), TOGGLE_MOUSE, KC_LEFT,  KC_DOWN,  KC_RGHT),
+        // ^^^ TUTAJ ZMIANA: Zamiast KC_RCTL jest TOGGLE_MOUSE ^^^
 
     /* MAC_FN */
     [MAC_FN] = LAYOUT_ansi_82(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,       TOGGLE_MOUSE,
+        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,                                            _______,                                _______,  _______,    _______,  _______,  _______,  _______),
 
@@ -61,35 +58,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                            KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                            KC_SPC,                                 KC_RALT,  MO(WIN_FN), TOGGLE_MOUSE, KC_LEFT,  KC_DOWN,  KC_RGHT),
+        // ^^^ TUTAJ ZMIANA: Zamiast KC_RCTL jest TOGGLE_MOUSE ^^^
 
     /* WIN_FN */
     [WIN_FN] = LAYOUT_ansi_82(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,       TOGGLE_MOUSE,
+        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,                                            _______,                                _______,  _______,    _______,  _______,  _______,  _______),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        // 1. Obsluga wlacznika (Fn + Home)
         case TOGGLE_MOUSE:
             if (record->event.pressed) {
                 mouse_mode_active = !mouse_mode_active;
             }
             return false;
 
-        // 2. Obsluga klawisza F
         case KC_F:
             if (mouse_mode_active) {
                 if (record->event.pressed) {
-                    tap_code(KC_F);             // Kliknij F
-                    register_code(KC_MS_BTN1);  // Teraz kompilator na 100% wie co to jest
+                    tap_code(KC_F);
+                    register_code(KC_MS_BTN1);
                 } else {
-                    unregister_code(KC_MS_BTN1); // Teraz kompilator na 100% wie co to jest
+                    unregister_code(KC_MS_BTN1);
                 }
                 return false;
             }
