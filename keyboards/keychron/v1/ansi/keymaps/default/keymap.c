d096cac4a88eeb01072e44fb68849e4e1c421ed1
cntr1337 <blaszczynski.konrad@gmail.com>
2026-01-12 14:56:24 +0100

    Refactor keymap for mouse functionality
    Updated keymap to replace TOGGLE_MOUSE with KC_MS_BTN1 and modified mouse mode logic.

--- keyboards/keychron/v1/ansi/keymaps/default/keymap.c
+++ keyboards/keychron/v1/ansi/keymaps/default/keymap.c
@@ -8,7 +8,7 @@
 
 #include QMK_KEYBOARD_H
 
-/* FIX NAPRAWIAJACY BLAD KOMPILACJI (MYSZKA) */
+/* FIX MYSZKI - Definicja na sztywno */
 #ifndef KC_MS_BTN1
 #define KC_MS_BTN1 0x00F9
 #endif
@@ -22,10 +22,6 @@ enum layers{
     WIN_FN
 };
 
-enum custom_keycodes {
-    TOGGLE_MOUSE = SAFE_RANGE,
-};
-
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
@@ -36,11 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_DEL,             KC_INS,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_MS_BTN1, /* Test: PGDN to Mysz */
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LOPT,  KC_LCMD,                                            KC_SPC,                                 KC_RCMD,  MO(MAC_FN), TOGGLE_MOUSE, KC_LEFT,  KC_DOWN,  KC_RGHT),
-        // ^^^ TUTAJ ZMIANA: Zamiast KC_RCTL jest TOGGLE_MOUSE ^^^
+        KC_LCTL,  KC_LOPT,  KC_LCMD,                                            KC_SPC,                                 KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     /* MAC_FN */
     [MAC_FN] = LAYOUT_ansi_82(
@@ -55,11 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_INS,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_MS_BTN1, /* Test: PGDN to Mysz */
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                            KC_SPC,                                 KC_RALT,  MO(WIN_FN), TOGGLE_MOUSE, KC_LEFT,  KC_DOWN,  KC_RGHT),
-        // ^^^ TUTAJ ZMIANA: Zamiast KC_RCTL jest TOGGLE_MOUSE ^^^
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                            KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     /* WIN_FN */
     [WIN_FN] = LAYOUT_ansi_82(
@@ -73,12 +67,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case TOGGLE_MOUSE:
+        // 1. Logika: Prawy Ctrl + Plus (KC_EQL)
+        case KC_EQL: // To jest klawisz z Plusem i Równa Się
             if (record->event.pressed) {
-                mouse_mode_active = !mouse_mode_active;
+                // Sprawdzamy, czy PRAWY CTRL jest wcisniety
+                if (get_mods() & MOD_BIT(KC_RCTL)) {
+                    // Jesli tak - przelaczamy tryb
+                    mouse_mode_active = !mouse_mode_active;
+                    return false; // Nie wpisujemy znaku "=" ani "+", tylko przelaczamy
+                }
             }
-            return false;
+            return true; // Jesli Ctrl nie jest wcisniety, dziala normalnie
 
+        // 2. Obsluga klawisza F
         case KC_F:
             if (mouse_mode_active) {
                 if (record->event.pressed) {
