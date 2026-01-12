03d103b3712893d4d8985e29ac6fc529b153b694
cntr1337 <blaszczynski.konrad@gmail.com>
2026-01-12 15:43:43 +0100

    Update rules.mk

--- keyboards/keychron/v1/ansi/rules.mk
+++ keyboards/keychron/v1/ansi/rules.mk
@@ -1,3 +1,15 @@
-MOUSEKEY_ENABLE = yes
-VIA_ENABLE = yes
+# Optymalizacja (naprawia bledy dziwnego zachowania)
 LTO_ENABLE = yes
+
+# Wlaczenie obslugi VIA (zeby program VIA widzial klawiature)
+VIA_ENABLE = yes
+
+# Wlaczenie myszki (Nasz glowny cel)
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+
+# Wlaczenie przelacznika Mac/Windows (BEZ TEGO KLAWIATURA WARIUJE I PISZE QWERT)
+DIP_SWITCH_ENABLE = yes
+
+# Wlaczenie pelnego N-Key Rollover (zeby mozna bylo wciskac duzo klawiszy naraz)
+NKRO_ENABLE = yes
