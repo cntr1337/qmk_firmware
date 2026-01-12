054f82e286bb95f830e64e96ee040f457a66a9d4
cntr1337 <blaszczynski.konrad@gmail.com>
2026-01-12 15:05:40 +0100

    Update rules.mk

--- keyboards/keychron/v1/ansi/rules.mk
+++ keyboards/keychron/v1/ansi/rules.mk
@@ -1,13 +1,2 @@
-MCU = STM32L432
-BOOTLOADER = stm32-dfu
-
-# Wlaczenie funkcji myszki - BEZ TEGO NIE ZADZIALA
 MOUSEKEY_ENABLE = yes
-EXTRAKEY_ENABLE = yes
-
-# Obsluga VIA (zeby widzialo klawiature)
 VIA_ENABLE = yes
-
-# Wylaczenie zbednych funkcji zeby zaoszczedzic miejsce
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
